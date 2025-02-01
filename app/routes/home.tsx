e07b8878390963a704653b742f219efba8f28924
Brian Cilenti <briancilenti@gmail.com>
2025-02-01 12:33:26 -0500

    spike: ai sdk

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,6 @@
 import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
+import { useFetcher } from "react-router";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -13,12 +14,29 @@ export async function loader({ context }: Route.LoaderArgs) {
 }
 
 export default function Home({ loaderData }: Route.ComponentProps) {
+  const fetcher = useFetcher();
+
+  console.log("\n", `fetcher.data = `, fetcher.data, "\n");
   return (
-    <div
-      className="flex items-center justify-center pt-16 pb-4 text-3xl"
-      data-testid="home"
-    >
+    <div className="flex flex-col items-center gap-2 pt-8 justify-center">
       Hello Home
+      <fetcher.Form method="post" action="/resources/alpha">
+        {/* submit button */}
+        <button
+          type="submit"
+          className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-opacity-50"
+        >
+          Generate Text
+        </button>
+      </fetcher.Form>
+      {/* loading state */}
+      {fetcher.state !== "idle" ? (
+        <p>Loading...</p>
+      ) : fetcher?.data?.text ? (
+        fetcher?.data?.text
+      ) : (
+        <p>Click the button to generate text</p>
+      )}
     </div>
   );
 }
--- app/routes/resources/alpha.ts
+++ app/routes/resources/alpha.ts
@@ -2,6 +2,8 @@ import type { Route } from "./+types/alpha";
 import { openai } from "@ai-sdk/openai";
 import { generateText } from "ai";
 
+export type { Route as GenerateTextType } from "./+types/alpha";
+
 export async function action({ context }: Route.ActionArgs) {
   const { text } = await generateText({
     model: openai("gpt-4-turbo"),
