fe5d050a5bc12f2fb7a54538a15969ad527ae0bb
Brian Cilenti <briancilenti@gmail.com>
2025-02-01 15:40:41 -0500

    spike: ai sdk

--- app/routes/layout-one.tsx
+++ app/routes/layout-one.tsx
@@ -2,9 +2,11 @@ import { Outlet } from "react-router";
 
 export default function Dashboard() {
   return (
-    <div>
-      {" "}
-      Hey, welcome to the dashboard page! <Outlet />
+    <div className="container bg-green-800 p-4">
+      Hey, welcome to the dashboard page!
+      <div className="flex flex-col items-center gap-2">
+        <Outlet />
+      </div>
     </div>
   );
 }
